3d325756121b8fbd83c95fe14b5e33eda91060d2
Masterplexus <nospamming@web.de>
2014-11-05 18:41:00 +0100

    zum Commit vorgemerkte Änderungen: 	geändert:               BoardConfig.mk 	neue Datei:             BoardConfig.mk.old 	umbenannt:              kernel -> Kernel/kernel 	neue Datei:             README 	geändert:               cm.mk 	geändert:               device_gti8200n.mk 	neue Datei:             full_gti8200n.mk 	neue Datei:             lib/modules/cci_datastub.ko 	neue Datei:             lib/modules/ccinetdev.ko 	neue Datei:             lib/modules/cidatattydev.ko 	neue Datei:             lib/modules/citty.ko 	neue Datei:             lib/modules/cmmb_mxd.ko 	neue Datei:             lib/modules/cploaddev.ko 	neue Datei:             lib/modules/diag.ko 	neue Datei:             lib/modules/galcore.ko 	neue Datei:             lib/modules/geu.ko 	neue Datei:             lib/modules/gs_diag.ko 	neue Datei:             lib/modules/hwmap.ko 	neue Datei:             lib/modules/mbt8xxx.ko 	neue Datei:             lib/modules/mlan.ko 	neue Datei:             lib/modules/msocketk.ko 	neue Datei:             lib/modules/physrw.ko 	neue Datei:             lib/modules/scsi_wait_scan.ko 	neue Datei:             lib/modules/sd8xxx.ko 	neue Datei:             lib/modules/seh.ko 	neue Datei:             lib/modules/smsmdtv.ko 	neue Datei:             lib/modules/usimeventk.ko 	neue Datei:             recovery/extra.fstab 	neue Datei:             recovery/init.recovery.pxa988.rc 	neue Datei:             recovery/twrp.fstab 	neue Datei:             rootdir/Android.mk 	neue Datei:             rootdir/etc/extra.fstab 	neue Datei:             rootdir/etc/fstab.pxa988 	neue Datei:             rootdir/etc/lpm.rc 	neue Datei:             rootdir/etc/ueventd.pxa988.rc 	neue Datei:             system/lib/modules/cci_datastub.ko 	neue Datei:             system/lib/modules/ccinetdev.ko 	neue Datei:             system/lib/modules/cidatattydev.ko 	neue Datei:             system/lib/modules/citty.ko 	neue Datei:             system/lib/modules/cmmb_mxd.ko 	neue Datei:             system/lib/modules/cploaddev.ko 	neue Datei:             system/lib/modules/diag.ko 	neue Datei:             system/lib/modules/galcore.ko 	neue Datei:             system/lib/modules/geu.ko 	neue Datei:             system/lib/modules/gs_diag.ko 	neue Datei:             system/lib/modules/hwmap.ko 	neue Datei:             system/lib/modules/mbt8xxx.ko 	neue Datei:             system/lib/modules/mlan.ko 	neue Datei:             system/lib/modules/msocketk.ko 	neue Datei:             system/lib/modules/physrw.ko 	neue Datei:             system/lib/modules/scsi_wait_scan.ko 	neue Datei:             system/lib/modules/sd8xxx.ko 	neue Datei:             system/lib/modules/seh.ko 	neue Datei:             system/lib/modules/smsmdtv.ko 	neue Datei:             system/lib/modules/usimeventk.ko

--- device_gti8200n.mk
+++ device_gti8200n.mk
@@ -5,6 +5,11 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/gti8200n/gti8200n-vendor.mk)
 
+# Enable higher-res drawables while keeping mdpi as primary source (used from T110)
+PRODUCT_AAPT_CONFIG := large mdpi hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+PRODUCT_LOCALES += mdpi
+
 DEVICE_PACKAGE_OVERLAYS += device/samsung/gti8200n/overlay
 
 LOCAL_PATH := device/samsung/gti8200n
@@ -16,6 +21,22 @@ endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
+
+#create a directory in /system/
+#    $(shell mkdir -p $(TARGET_OUT)out/target/product/gti8200n/recovery/root/lib/)
+#    $(shell mkdir -p $(TARGET_OUT)out/target/product/gti8200n/recovery/root/lib/modules)
+#copy stuff
+#    $(shell cp $(LOCAL_PATH)/lib/modules/* `pwd`/$(TARGET_OUT)out/target/product/gti8200n/recovery/root/lib/modules)
+#PRODUCT_COPY_FILES += \ $(LOCAL_PATH)/lib/*:recovery/root/lib/ 
+#PRODUCT_COPY_FILES += \ $(LOCAL_PATH)/lib/modules/*:recovery/root/lib/modules/* 
+
+# Init files (used from T110)
+PRODUCT_PACKAGES += \
+    fstab.pxa988 \
+    init_charging.rc \
+    ueventd.pxa988.rc \
+    lpm.rc \
+    init.recovery.pxa988.rc
 
 $(call inherit-product, build/target/product/full.mk)
 
